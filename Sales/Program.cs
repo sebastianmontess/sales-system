cc61f23f478295eccc0c409d539a5d26721e1e62
junor <junor@LAPTOP-4PE007C1>
2021-02-19 03:22:51 -0500

    pryecto

---
+++ Sales/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Seller e1 = new Seller();
+            e1.Name = "Juan";
+            e1.Age = 25;
+        }
+    }
+}
---
+++ Sales/Seller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales
+{
+    class Seller
+    {
+        public string Name;
+        public int Age;
+    }
+}
