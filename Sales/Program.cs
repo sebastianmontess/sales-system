18a502e66c6495067256e945bae329260bc21abe
junor <junor@LAPTOP-4PE007C1>
2021-03-02 23:42:36 -0500

    first commit

--- Sales/Program.cs
+++ Sales/Program.cs
@@ -6,51 +6,84 @@ namespace Sales
     {
         static void Main(string[] args)
         {
+           Total total = new Total();
+
+            //sellers
             Seller e1 = new Seller();
             e1.Name = "Juan";
             e1.Age = 25;
+            total.AddSeller(e1);
 
+            Seller e2 = new Seller();
+            e1.Name = "Sebastian";
+            e1.Age = 35;
+            total.AddSeller(e2);
+
+            Seller e3 = new Seller();
+            e1.Name = "Daniela";
+            e1.Age = 20;
+            total.AddSeller(e3);
+
+            
+            //products
             Product product1 = new Product();
             product1.Name = "Iphone";
-            product1.Price = 1000000d;
+            product1.Price = 300;
+            total.AddProducts(product1);
 
             Product product2 = new Product();
             product2.Name = "Samsung";
-            product2.Price = 500000d;
+            product2.Price = 500;
+            total.AddProducts(product2);
 
             Product product3 = new Product();
             product3.Name = "Huawei";
-            product3.Price = 250000d;
+            product3.Price = 100;
+            total.AddProducts(product2);
 
-           
 
+            //sales
             Sale V1 = new Sale();
             V1.Product = product1;
             V1.Seller = e1;
             V1.Comments = "New client";
+            total.Addsales(V1);
 
             Sale V2 = new Sale();
             V2.Product = product2;
-            V2.Seller = e1;
+            V2.Seller = e2;
             V2.Comments = "Regular client";
+            total.Addsales(V2);
+
 
             Sale V3 = new Sale();
             V3.Product = product3;
-            V3.Seller = e1;
+            V3.Seller = e3;
             V3.Comments = "New client";
-
-           
-            double  count = V1.Product.Price + V2.Product.Price + V3.Product.Price;
-
-            System.Console.WriteLine("Sales total amount: " + count );
+            total.Addsales(V3);
 
 
 
+            double count = V1.Product.Price + V2.Product.Price + V3.Product.Price;
+
+            //  System.Console.WriteLine("Sales total amount: " + count );
 
 
+            //the seller who sold the cheapest product
+
+            System.Console.WriteLine("The cheapest product was sold by " + total.seller_economic());
 
 
+            //Average price
 
+            System.Console.WriteLine(" The average value of products is " + total.Average_Products());
+
+            //Units of product sold
+
+            System.Console.WriteLine("The quantity of sales is " + total.salesCount);
+
+            //most expensive product
+            Console.WriteLine("The product more expensive is " + total.expensive_Product());
         }
     }
 }
---
+++ Sales/Total.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales
+{
+    class Total
+    {
+        Seller[] Sellers = new Seller[10];
+        int sellerCount = 0;
+
+        Product[] Products = new Product[30];
+        public int productsCount = 0;
+
+        Sale[] Sales = new Sale[800];
+        public int salesCount = 0;
+
+
+        public void AddSeller(Seller newSeller)
+        {
+            Sellers[sellerCount] = newSeller;
+            sellerCount++;
+        }
+
+
+        public void AddProducts(Product newProduct)
+        {
+            Products[productsCount] = newProduct;
+            productsCount++;
+        }
+
+
+        public void Addsales(Sale newSale)
+        {
+            Sales[salesCount] = newSale;
+            salesCount++;
+        }
+
+
+        public string seller_economic()
+        {
+            string nameSellerProduct = "";
+
+            double economic = Products[0].Price;
+            for (int i = 0; i <= salesCount - 1; i++)
+            {
+
+                if (economic < Products[i].Price)
+                {
+                    economic = Products[i].Price;
+                }
+                else
+                {
+                    nameSellerProduct = Sales[i].Seller.Name;
+                }
+            }
+
+            return nameSellerProduct;
+        }
+
+
+        public double Average_Products()
+        {
+            double sum = 0;
+
+            for (int i = 0; i <= productsCount -1 ; i++)
+            {
+                if (Products[i].Price != 0)
+                {
+                   sum += Products[i].Price;
+                }
+            }
+
+            sum = sum / productsCount;
+
+            return sum;
+        }
+
+
+        public double ValueOfProducts()
+        {
+            double countValueOfProducts = 0;
+
+            for (int i = 0; i <= productsCount - 1; i++)
+            {
+                if (Products[i].Price != 0)
+                {
+                    countValueOfProducts += Products[i].Price;
+                }
+            }
+
+            return countValueOfProducts;
+        }
+
+        public string expensive_Product()
+        {
+            string nameProduct = "";
+
+            double expensive = Products[0].Price;
+            for (int i = 0; i <= productsCount - 1; i++)
+            {
+
+                if (expensive > Products[i].Price)
+                {
+                    expensive = Products[i].Price;
+                }
+                else
+                {
+                    nameProduct = Products[i].Name;
+                }
+            }
+
+            return nameProduct;
+        }
+
+    }
+}
